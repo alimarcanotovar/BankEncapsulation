ab8cd17eccfe5a8fb2b0143ebf17f646091b12fe
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-08-19 14:05:38 -0500

    (answers)

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double money)
+        {
+            _balance += money;
+        }
+
+        public void Withdraw(double money)
+        {
+            _balance -= money;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+
+
+    }
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount();
+
+            Console.WriteLine("How much do you want to deposit");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            myAccount.Deposit(amountToDeposit);
+
+            myAccount.Deposit(100);
+
+            Console.WriteLine(myAccount.GetBalance());
+
+            Console.WriteLine("How much do you want to withdraw");
+            var amountTowithdraw = double.Parse(Console.ReadLine());
+            
+
+            myAccount.Withdraw(amountTowithdraw);
+            Console.WriteLine(myAccount.GetBalance());
+
+
+        }
+    }
+}
